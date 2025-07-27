d4314edb26f2aacf0ce2f4162745bb8acc4398c7
Juan Sotomayor <juangslopes@gmail.com>
2025-07-27 11:13:37 -0300

    Atualizando texto e estrutura

---
+++ Teste de Fisher/Teste_Fisher.py
@@ -0,0 +1,45 @@
+import streamlit as st
+
+# Título
+
+st.title("Teste Exato de Fisher")
+
+# Seção Inicial (Conceito)
+## O que é? Quando surgiu?
+st.write("O Teste Exato de Fisher - ou apenas Teste de Fisher - é um  "
+"**Teste de Hipótese**, que é usado para avaliar a associação entre duas (ou mais) variáveis categóricas.")
+
+st.write("Ou seja, ele nos permite ver se, diante de duas situações ou tratamentos, há alguma interferência nos dois resultados possíveis.")
+st.write("Imagine a seguinte situação:" 
+         " Uma empresa de bolos deseja ver se ao colocar a palavra \'caseiro\' na embalagem de seus bolos, ocorre algum impacto positivo no seu número de vendas."
+         " Para isso, o estatístico contratado resolveu avaliar da seguinte forma:"
+         " Para as 100 pessoas que forem expostas às embalagens A e B, devemos avaliar se o bolo foi ou não comprado"
+         " A embalagem A é a padrão que já está em circulação e já está inserida no mercado, enquanto a embalagem B é idêntica à embalagem A, sua única diferença é a menção à receita caseira." \
+)
+
+st.write("Para a empresa, é importante saber se existe um impacto positivo ou não em suas vendas, porque se não houver diferença na embalagem, terá sido um investimento perdido, ou no pior dos casos, poderá perder clientes e sofrer uma queda no número de vendas.")
+
+
+
+
+### É possível um hyperlink para um outro streamlit de Teste de Hipótese? 'Saiba mais'
+## Utilidade?
+
+## Quais áreas isso é/pode ser aplicado?
+
+
+
+# Seção Principal (Como funciona: Suposições e Algoritmo)
+## Suposições
+
+## Estrutura de Teste de Hipótese
+
+## Passo a passo teórico (Algoritmo)
+
+
+
+# Seção Final (Exemplos aplicados)
+## Exemplo 1
+
+## Exemplo 2
+# Bibliografia
