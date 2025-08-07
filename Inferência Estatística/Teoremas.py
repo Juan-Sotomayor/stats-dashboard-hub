368d464e82b18339deb439df834b444e5acddf2e
Juan Sotomayor <juangslopes@gmail.com>
2025-08-06 22:52:27 -0300

    Criar pasta para Inferência

---
+++ Inferência Estatística/Teoremas.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.write("Teoremas importantes para Inferência Estatística")
+
+# Teorema Lehmann-Scheffé
+# Teorema Raomann-Blackwell
+# Teorema Limite Inferior de CR
+
+#
